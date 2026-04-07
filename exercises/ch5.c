056dccd2e60a7cf4b82812418a9f14d0eadf2ba4
ssd4408 <69972947+ssd4408@users.noreply.github.com>
2026-04-07 18:05:03 +0900

    Create ch5.c

---
+++ exercises/ch5.c
@@ -0,0 +1,132 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*
+//ex1 tmeperature
+int main()
+{
+  int temperature;
+  printf("enter tempeatre :");
+  scanf("%d", &temperature);
+  
+  if(temperature > 0)
+    printf("above zero");
+  else
+    printf("below zero"):
+  
+  return 0;
+}
+*/
+
+
+
+/*
+//ex2 detect even number
+int main()
+{
+  int num;
+  printf("enter number");
+  scanf("%d", &num);
+
+  if(num % 2 == 0)
+    printf("even number");
+  else
+    printf("odd number");
+
+  return 0;
+{
+*/
+
+/*
+//ex3 grade
+int main()
+{
+  float grade;
+  printf("enter your grade :");
+  scanf("%f", &grade);
+  if (grade > 90)
+    printf("A");
+  else if (grade > 80)
+    printf("B");
+  else if (grade > 70)
+    printf("C");
+  else if (grade > 60)
+    printf("D");
+  else if (grade > 50)
+    printf("E");
+  else
+    printf("F");
+
+  return 0;
+}
+*/
+
+/*
+//ex4 __?__:__
+int main()
+{
+  int x, y;
+  printf("enter first number");
+  scanf("%d", &x);
+  printf("enter second number");
+  scanf("%d", &y);
+
+  printf("the bigger number : %d", (x>y)?x:y);
+  printf("the smaller number: %d", (x<y)?x:y);
+
+  return 0;
+}
+*/
+
+/*
+//ex5 coin throwing
+#include <time.h>
+int main()
+{
+  int coin;
+  srand(time(NULL));
+  coin = srand() % 2;
+
+  if (coin == 0)
+    printf("front");
+  else
+    printf("behind");
+
+  return 0;
+}
+*/
+
+/*
+//ex6 leep year
+int main()
+{
+  int year;
+  printf("enter year :");
+  scanf("%d", &year);
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+    printf("leep year");
+  else
+    printf("not");
+  return 0;
+}
+*/
+
+/*
+//ex7 find biggest number
+int main()
+{
+  int a, b, c;
+  printf("enter three number :");
+  scanf("%d", &a, &b, &c);
+  printf("%d",((a>b)?a:b)>c?((a>b)?a:b):c);
+  return 0;
+}
+*/
+
+
+
+
+
+
+
+
